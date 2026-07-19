21ef478625026e82b8f69f8061e4668e64172035
Mike <mgonzalezobenza@gmail.com>
2026-07-19 11:39:53 +0200

    hqjerhkjqe

--- csharp/kata.test/CharacterTests.cs
+++ csharp/kata.test/CharacterTests.cs
@@ -5,42 +5,70 @@ public class CharacterTests
     [Fact]
     public void DoDamageToCharacter_StillAlive()
     {
-        var character = new Character();
+        var defender = new Character();
+        var attacker = new Character();
 
-        character.DoDamage(100);
+        attacker.DoDamage(defender, 100);
 
-        character.alive.Should().BeTrue();
+        defender.alive.Should().BeTrue();
     }
 
     [Fact]
     public void DoDamageToCharacter_NotAlive()
     {
-        var character = new Character();
+        var defender = new Character();
+        var attacker = new Character();
 
-        character.DoDamage(2000);
+        attacker.DoDamage(defender, 2000);
 
-        character.alive.Should().BeFalse();
+        defender.alive.Should().BeFalse();
     }
 
     [Fact]
     public void HealCharacter_ToFull()
     {
-        var character = new Character();
+        var defender = new Character();
+        var attacker = new Character();
 
-        character.DoDamage(500);
-        character.Heal(1000);
+        attacker.DoDamage(defender, 500);
+        defender.Heal(1000);
 
-        character.health.Should().Be(Character.maxHealth);
+        defender.health.Should().Be(Character.maxHealth);
     }
 
     [Fact]
     public void CannotHealDeadCharacter()
     {
-        var character = new Character();
+        var defender = new Character();
+        var attacker = new Character();
 
-        character.DoDamage(1000);
-        character.Heal(1000);
+        attacker.DoDamage(defender, 1000);
+        defender.Heal(1000);
 
-        character.alive.Should().BeFalse();
+        defender.alive.Should().BeFalse();
+    }
+
+    [Fact]
+    public void DoDamageToStrongerCharacter()
+    {
+        var attacker = new Character();
+        var defender = new Character();
+        defender.level = 6;
+
+        attacker.DoDamage(defender, 100);
+
+        defender.health.Should().Be(Character.maxHealth - 50);
+    }
+
+    [Fact]
+    public void DoDamageToWeakerCharacter()
+    {
+        var attacker = new Character();
+        var defender = new Character();
+        attacker.level = 6;
+
+        attacker.DoDamage(defender, 100);
+
+        defender.health.Should().Be(Character.maxHealth - 150);
     }
 }
--- csharp/kata/Character.cs
+++ csharp/kata/Character.cs
@@ -14,15 +14,46 @@ public class Character
         alive = true;
     }
 
-    public void DoDamage(int damage)
+    public void DoDamage(Character character, int damage)
     {
-        health = Math.Max(0, health - damage);
-        alive = health > 0;
+        if (character == this)
+        {
+            throw new InvalidOperationException("No se puede hacer daño a si mismo.");
+        }
+
+        damage = CalculateDamage(character, damage);
+
+        character.health = Math.Max(0, character.health - damage);
+        character.alive = character.health > 0;
     }
 
     public void Heal(int heal)
     {
         if (alive)
             health = Math.Min(health + heal, maxHealth);
+    }
+
+    private int CalculateDamage(Character character, int damage)
+    {
+        if (isWeakerThan(character))
+        {
+            return (int)(damage * 0.5);
+        }
+        if (isStrongerThan(character))
+        {
+            return (int)(damage * 1.5);
+        }
+
+        return damage;
+    }
+
+    private bool isStrongerThan(Character character)
+    {
+        return character.level + 5 <= this.level;
+    }
+
+    private bool isWeakerThan(Character character)
+    {
+        return character.level >= this.level + 5;
     }
 }
