22efaf08ed0cde9ad2e96aa54dce4c2fafa48a30
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-07-19 11:05:09 +0200

    sdfsdfsd

---
+++ csharp/kata.test/CharacterTests.cs
@@ -0,0 +1,46 @@
+﻿namespace kata.test;
+
+public class CharacterTests
+{
+    [Fact]
+    public void DoDamageToCharacter_StillAlive()
+    {
+        var character = new Character();
+
+        character.DoDamage(100);
+
+        character.alive.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DoDamageToCharacter_NotAlive()
+    {
+        var character = new Character();
+
+        character.DoDamage(2000);
+
+        character.alive.Should().BeFalse();
+    }
+
+    [Fact]
+    public void HealCharacter_ToFull()
+    {
+        var character = new Character();
+
+        character.DoDamage(500);
+        character.Heal(1000);
+
+        character.health.Should().Be(1000);
+    }
+
+    [Fact]
+    public void CannotHealDeadCharacter()
+    {
+        var character = new Character();
+
+        character.DoDamage(1000);
+        character.Heal(1000);
+
+        character.alive.Should().BeFalse();
+    }
+}
---
+++ csharp/kata/Character.cs
@@ -0,0 +1,27 @@
+﻿namespace kata;
+
+public class Character
+{
+    public int level;
+    public int health;
+    public bool alive;
+
+    public Character()
+    {
+        level = 1;
+        health = 1000;
+        alive = true;
+    }
+
+    public void DoDamage(int damage)
+    {
+        health = Math.Max(0, health - damage);
+        alive = health > 0;
+    }
+
+    public void Heal(int heal)
+    {
+        if (alive)
+            health = Math.Min(health + heal, 1000);
+    }
+}
